f5e9d7a1c0fafd5fcce22693c95e1577b8e80b9d
Kyscen <charles.desbonne.2003@outlook.fr>
2025-01-09 19:25:09 +0100

    Setup

---
+++ src/main/java/net/kyscen/hellomod/HelloModClient.java
@@ -0,0 +1,10 @@
+package net.kyscen.hellomod;
+
+import net.fabricmc.api.ClientModInitializer; ;
+public class HelloModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kyscen/hellomod/HelloModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kyscen.hellomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HelloModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
