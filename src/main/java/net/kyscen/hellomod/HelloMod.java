1f13ac8b9d1b333d8184c3b79bc4a285316ad097
Kyscen <charles.desbonne.2003@outlook.fr>
2025-01-09 20:01:21 +0100

    First Items + language

--- src/main/java/net/kyscen/hellomod/HelloMod.java
+++ src/main/java/net/kyscen/hellomod/HelloMod.java
@@ -1,18 +1,19 @@
 package net.kyscen.hellomod;
 
 import net.fabricmc.api.ModInitializer;
+import net.kyscen.hellomod.item.ModItemGroups;
+import net.kyscen.hellomod.item.ModItems;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloMod implements ModInitializer {
 	public static final String MOD_ID = "hellomod";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems(); 
 	}
 }
